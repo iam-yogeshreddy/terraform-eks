d5d40e71added601d0f94e1ac3f65d3233fd04f1
Yogesh <136956748+iam-yogeshreddy@users.noreply.github.com>
2025-07-06 10:56:42 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,30 +1,29 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.8.0"
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.6.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
+      version = "~> 2.29.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = "~> 5.51.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.1.0"
+      version = "~> 2.5.1"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.1.0"
+      version = "~> 3.2.2"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = "~> 2.3.4"
     }
   }
 }
